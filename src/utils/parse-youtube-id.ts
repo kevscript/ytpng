53c7c2c77e7b39894b891e4d9773b473db13217f
Kevin Ostafinski <kevscripted@gmail.com>
2024-04-25 14:37:16 +0200

    fix regex

--- src/utils/parse-youtube-id.ts
+++ src/utils/parse-youtube-id.ts
@@ -1,8 +1,6 @@
 export function parseYoutubeId(url: string): string | boolean {
-  const regexp: RegExp = new RegExp(
-    '(?:youtube(?:-nocookie)?.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu.be/)([^"&?/s]{11})',
-    "i"
-  );
+  const regexp =
+    /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
   const matches = url.match(regexp);
 
   return matches ? matches[1] : false;
